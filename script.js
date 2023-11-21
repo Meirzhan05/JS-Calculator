0d05c468eb15fa36765c071250a5deface259f73
Meirzhan05 <saparovmeirzhan@gmail.com>
2023-11-20 20:02:10 -0500

    Added calculation logic for only two numbers

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-let btns = document.querySelectorAll('.button');
-let num = document.querySelector('#num');
+const btns = document.querySelectorAll('.button');
+const num = document.querySelector('#num');
+const operators = '-+÷×';
+let numbers = [];
+let operator;
+
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
-        btnValue = btn.value;
-        textContent = num.textContent;
-        if ('0-+'.includes(textContent) || btnValue.includes('0-+')) {
-            num.textContent = btnValue;
-        } else if ((isNumeric(textContent) || btnValue == ".") && (isNumeric(btnValue) || btnValue == ".")) {
-            num.textContent += btnValue;
-        } else if (btnValue === "+/-") {
-            if (isNumeric(textContent[0])) {
-                num.textContent = "-" + textContent;
-            } else {
-                num.textContent = num.textContent.substring(1, textContent.length);
+        if (num.textContent.length <= 9) {
+            btnValue = btn.value;
+            textContent = num.textContent;
+
+            if (operators.includes(btnValue)) {
+                operator = btnValue;
+                numbers.push(parseFloat(textContent));
+                console.log(operator);
+            }
+
+            if (btnValue == "=") {
+                numbers.push(parseFloat(textContent));
+                num.textContent = calculate(numbers, operator);
+            } else if (btnValue == "AC") {
+                num.textContent = "0";
+            } else if (textContent != "+/-" && ('0+-÷×'.includes(textContent) || operators.includes(btnValue))) {
+                num.textContent = btnValue;
+            } else if ((isNumeric(textContent) || btnValue == ".") && (isNumeric(btnValue) || btnValue == ".")) {
+                num.textContent += btnValue;
+            } else if (btnValue === "+/-") {
+                if (isNumeric(textContent[0])) {
+                    num.textContent = "-" + textContent;
+                } else {
+                    num.textContent = num.textContent.substring(1, textContent.length);
+                } 
             }
         }
-    })
+    });
 });
 
 function isNumeric(str) {
     return !isNaN(str) && !isNaN(parseFloat(str));
+}
+
+function calculate(numbers, operator) {
+    let firstNumber = numbers[0];
+    let secondNumber = numbers[1];
+    let total;
+    switch (operator) {
+        case "+": 
+            total = firstNumber + secondNumber;    
+        case "-":
+            total = firstNumber - secondNumber;
+        case "÷":
+            total = firstNumber / secondNumber;
+        case "×": 
+            total = firstNumber * secondNumber;
+    }
+    return total;
 }
