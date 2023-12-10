08378ba0cb99e543458c6027d58282198e4a5f90
Meirzhan05 <saparovmeirzhan@gmail.com>
2023-12-10 13:20:54 -0500

    Fixed all arithmetic and logical errors

--- script.js
+++ script.js
@@ -1,58 +1,88 @@
 const btns = document.querySelectorAll('.button');
 const num = document.querySelector('#num');
-const operators = '-+÷×';
+const OPERATORS = '-+÷×';
 let numbers = [];
 let operator;
+const MAX_CHARS = 9;
+const CLEAR_BTN = "AC";
 
+// Logic of a calculator 
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
-        if (num.textContent.length <= 9) {
-            btnValue = btn.value;
-            textContent = num.textContent;
+        let btnValue = btn.value;
+        let textContent = num.textContent;
 
-            if (operators.includes(btnValue)) {
-                operator = btnValue;
-                numbers.push(parseFloat(textContent));
-                console.log(operator);
-            }
-
-            if (btnValue == "=") {
-                numbers.push(parseFloat(textContent));
-                num.textContent = calculate(numbers, operator);
-            } else if (btnValue == "AC") {
+        if (OPERATORS.includes(btnValue)) {
+            operator = btnValue;
+            numbers.push(parseFloat(textContent));
+        }
+        
+        if (btnValue == "=") {
+            numbers.push(parseFloat(textContent));
+            textContent = calculate(numbers, operator);
+            numbers = [];
+            num.textContent = textContent;
+        } else if (btnValue == CLEAR_BTN) {
+            num.textContent = "0";
+            numbers = [];
+        } else if ("+/-" != btnValue && btnValue != "." && ('0+-÷×'.includes(textContent) || OPERATORS.includes(btnValue))) {
+            num.textContent = btnValue;
+        } else if ((isNumeric(textContent) && isNumeric(btnValue) || (btnValue == "." && textContent[textContent.length - 1] != ".")) 
+        && num.textContent.length <= MAX_CHARS) {
+            num.textContent += btnValue;
+        } else if (btnValue === "+/-" && textContent != 0) {
+            if (isNumeric(textContent[0])) {
+                num.textContent = "-" + textContent;
+            } else {
+                num.textContent = textContent.substring(1, textContent.length);
+            } 
+        } else if (btnValue == "%") {
+            numbers.push(parseFloat(textContent));
+            textContent = calculate(numbers, operator, true);
+            numbers = [];
+            num.textContent = textContent;
+        } else if (btnValue == "↺") {
+            if (textContent.length - 1 >= 1) {
+                num.textContent = textContent.substring(0, textContent.length - 1);
+            } else {
                 num.textContent = "0";
-            } else if (textContent != "+/-" && ('0+-÷×'.includes(textContent) || operators.includes(btnValue))) {
-                num.textContent = btnValue;
-            } else if ((isNumeric(textContent) || btnValue == ".") && (isNumeric(btnValue) || btnValue == ".")) {
-                num.textContent += btnValue;
-            } else if (btnValue === "+/-") {
-                if (isNumeric(textContent[0])) {
-                    num.textContent = "-" + textContent;
-                } else {
-                    num.textContent = num.textContent.substring(1, textContent.length);
-                } 
             }
         }
     });
 });
 
+
 function isNumeric(str) {
     return !isNaN(str) && !isNaN(parseFloat(str));
 }
 
-function calculate(numbers, operator) {
+function calculate(numbers, operator, percent=false) {
     let firstNumber = numbers[0];
     let secondNumber = numbers[1];
     let total;
+    const limit = 1e9;
+
+    if (percent) {
+        secondNumber = secondNumber / 100;
+    }
+
     switch (operator) {
-        case "+": 
-            total = firstNumber + secondNumber;    
+        case "+":
+            total = firstNumber + secondNumber;  
+            break; 
         case "-":
             total = firstNumber - secondNumber;
+            break;
         case "÷":
             total = firstNumber / secondNumber;
+            break;
         case "×": 
             total = firstNumber * secondNumber;
+            break;
+    }
+
+    if (Math.abs(total) > limit) {
+        total = total.toExponential(2)
     }
     return total;
 }
